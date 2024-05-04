e964be9f17b87fae6fdfbf90cc56fa12a111345e
Asif Khan <ak3226519@gmail.com>
2024-05-04 17:34:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-apexcharts']
-    }
-  }
-}
